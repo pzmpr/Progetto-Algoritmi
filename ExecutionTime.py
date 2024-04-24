b285144045ff308d51cc454279b52312eedec128
pzmpr <114858090+pzmpr@users.noreply.github.com>
2024-04-24 16:23:26 +0200

    Update ExecutionTime.py

--- ExecutionTime.py
+++ ExecutionTime.py
@@ -1,6 +1,7 @@
 import time
 import random
 import math
+import numpy
 import matplotlib.pyplot as plt
 import QuickSelect
 import HeapSelect
@@ -58,18 +59,52 @@ if __name__=="__main__":
                         benchmark(n, n, HeapSelect.heapselect, risoluzione, 0.001),
                         benchmark(n, n, MedianOfMedians.median_of_medians_select, risoluzione, 0.001))
 
-# Plot
+# Plot (line of best fit)
 xs, ys1, ys2, ys3, ys4 = zip(*points)
-plt.scatter(xs, ys1)
-plt.scatter(xs, ys2)
-plt.scatter(xs, ys3)
-plt.scatter(xs, ys4)
-#plt.plot(dimensioni_input, tempi_medi, marker='o', linestyle='-')
-#plt.xscale('log')
-#plt.yscale('log')
-plt.xlabel('Dimensione dell\'input (n)')
-plt.ylabel('Tempo medio di esecuzione (secondi)')
-plt.title('Tempi medi di esecuzione')
-plt.grid(True)
-plt.show() 
+
+fig1, ax1 = plt.subplot()
+fig.subtitle("Tempo di esecuzione Randomized QuickSelect")
+ax1.plot(xs, ys1)
+# plt.scatter(xs, ys1)
+a1, b1 = np.polyfit(xs, ys1, 1)
+ax1.plot(xs, a1*xs+b1)
+ax1.xlabel('Dimensione dell\'input (n)')
+ax1.ylabel('Tempo medio di esecuzione (secondi)')
+ax1.grid(True)
+
+fig1, ax1 = plt.subplot()
+fig.subtitle("Tempo di esecuzione QuickSelect")
+ax2.plot(xs, ys2)
+# plt.scatter(xs, ys2)
+a2, b2 = np.polyfit(xs, ys2, 1)
+ax2.plot(xs, a2*xs+b2)
+ax2.xlabel('Dimensione dell\'input (n)')
+ax2.ylabel('Tempo medio di esecuzione (secondi)')
+ax2.grid(True)
+
+fig1, ax1 = plt.subplot()
+fig.subtitle("Tempo di esecuzione Randomized HeapSelect")
+ax3.plot(xs, ys3)
+# plt.scatter(xs, ys3)
+a3, b3 = np.polyfit(xs, ys3, 1)
+ax3.plot(xs, a3*xs+b3)
+ax3.xlabel('Dimensione dell\'input (n)')
+ax3.ylabel('Tempo medio di esecuzione (secondi)')
+ax3.grid(True)
+
+fig1, ax1 = plt.subplot()
+fig.subtitle("Tempo di esecuzione Randomized MedianOfMediansSelect")
+ax4.plot(xs, ys4)
+# plt.scatter(xs, ys4)
+a4, b4 = np.polyfit(xs, ys4, 1)
+ax4.plot(xs, a4*xs+b4)
+ax4.xlabel('Dimensione dell\'input (n)')
+ax4.ylabel('Tempo medio di esecuzione (secondi)')
+ax4.grid(True)
+
+plt.show()
 plt.close()
+
+# Scala logaritmica
+# plt.xscale('log')
+# plt.yscale('log')
