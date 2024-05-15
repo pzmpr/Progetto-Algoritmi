c0d8d57f6312e49b3e21929e0d043fa71eed2bc4
pzmpr <114858090+pzmpr@users.noreply.github.com>
2024-05-15 10:58:56 +0200

    Update ExecutionTime.py

--- ExecutionTime.py
+++ ExecutionTime.py
@@ -69,8 +69,8 @@ def randomized_select(a, p, r, i):
 # scambia il numero con l'ultimo elemento di a
 # infine richiama partition "normale"
 def randomized_partition(a, low, high):
-  i = random.randint(low, high-1)
-  a[high-1], a[i] = a[i], a[high-1]
+  i = random.randint(low, high)
+  a[high], a[i] = a[i], a[high]
   return partition(a, low, high)
 
 
